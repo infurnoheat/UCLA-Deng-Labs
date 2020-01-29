58c2920029909c45df92a38fd7b6eeca423d80f0
infurnoheat <stevenleeyu@gmail.com>
2020-01-28 22:09:12 -0800

    Modified Data

--- ComBat.R
+++ ComBat.R
@@ -16,13 +16,14 @@ library(AnnotationDbi)
 
 #setting up the data
 
-MODRAW = read.csv("82-Gene_Expression_Data.csv" , header = TRUE) #gene expression file
-MODPHENO = read.csv("82-Gene_Phenotype_Data.csv" , header = TRUE) #phenotype file
+MODRAW = read.csv("82-Gene_Expression_Data_Nameless.csv" , header = TRUE) #gene expression file
+MODPHENO = read.csv("82-Gene_Phenotype_Data_Nameless.csv" , header = TRUE) #phenotype file
 dat <- as.matrix("MODRAW")
 
 #executing ComBat
 
-newdata <- ComBat(dat = dat , batch = MODPHENO$Batch , par.prior = FALSE , mod = null)
+newdata <- ComBat(dat = dat , batch = MODPHENO$Batch , par.prior = FALSE , mod = NULL)
 
 #writing into a file
+
 write.csv(newdata , file = "SampleData_ComBat.csv")
